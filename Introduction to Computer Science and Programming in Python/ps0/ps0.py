4f57b9c840dc65218a60769601ae344416d51768
Angel Cruz <131916836+angelabelcruz01@users.noreply.github.com>
2024-01-03 10:56:50 -0500

    Add files via upload

---
+++ Introduction to Computer Science and Programming in Python/ps0/ps0.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 26 09:35:03 2023
+
+@author: angelcruz
+"""
+
+import math 
+
+x = float(input("Please enter number x: "))
+y = float(input("Please enter number y: "))
+print('x**y = {}'.format(x**y))
+print('log(base 2) x = {}'.format(math.log(x,2)))
